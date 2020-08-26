385399808c50113a1943b5f32a44b95fc7b36f5c
alkihuri <alkihuri@gmail.com>
2020-08-26 10:21:07 +0300

    26/08/20
    Скрипт для загрузки видео

---
+++ Assets/Scripts/UI/VideoLoader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoLoader : MonoBehaviour
+{
+    public string uniqePath;
+    public VideoPlayer vp;
+
+    public bool letter, syllabels, word, image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (letter)
+            uniqePath = "/Letters/"  + GameStates.lettersOfLevel.Replace(",", "");
+        if (syllabels)
+            uniqePath = GameStates.syllablesOfLevel;
+        if (word)
+            uniqePath = GameStates.wordsOfLevel;
+        if(image)
+            uniqePath = GameStates.wordsOfLevel;
+
+        
+       
+
+        uniqePath = "Video" + uniqePath;
+        Debug.Log(uniqePath);
+        vp.clip = Resources.Load<VideoClip>(uniqePath);
+        vp.Play();
+    }
+
+    private void Update()
+    {
+        Debug.Log(((int)vp.frameCount).ToString()  + "/"  +  ((int)vp.frame).ToString());
+
+        if (((int)vp.frameCount - (int)vp.frame)<2)
+        {
+            GameObject.FindObjectOfType<SceneController>().LoadScene(GameObject.FindObjectOfType<SceneController>().next);
+        }
+    }
+}
